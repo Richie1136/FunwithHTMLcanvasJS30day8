ecf69716278660b95c020b6accfbea561139292e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-01 11:41:04 -0400

    Able to draw on the canvas using my ctx variable to change the line width and add the path

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
+ctx.lineWidth = 100
 
 let isDrawing = false
 
@@ -16,10 +17,19 @@ let lastY = 0
 
 const draw = (e) => {
   if (!isDrawing) return
-  console.log(e)
+  ctx.beginPath()
+  ctx.moveTo(lastX, lastY)
+  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.stroke()
+  lastX = e.offsetX
+  lastY = e.offsetY
 }
 
 canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mousedown', () => isDrawing = true)
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true
+  lastX = e.offsetX
+  lastY = e.offsetY
+})
 canvas.addEventListener('mouseup', () => isDrawing = false)
 canvas.addEventListener('mouseout', () => isDrawing = false)
